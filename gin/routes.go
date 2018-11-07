cf27462b2308f9b683c640cc9ce4332b03ea6476
silso <chroelo@gmail.com>
2018-11-06 19:48:41 -0600

    the wrong approach, commit this and trying another

--- gin/routes.go
+++ gin/routes.go
@@ -2,37 +2,36 @@ package gin
 
 import (
 	//"github.com/sezzle-calculator/config"
-	//"time"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	//cors "github.com/itsjamie/gin-cors"
 
 	"github.com/gin-gonic/contrib/static"
-	"html"
+	"io"
 	"fmt"
 )
 
+const MATH_LOG_SIZE = 10
+var mathLog [MATH_LOG_SIZE]string
+var mathLogIdx int
+var mathLogUpdated bool
+
 // InitRoutes : Creates all of the routes for our application and returns a router
 func InitRoutes() *gin.Engine {
+	mathLogIdx = 0
+	mathLogUpdated = false
 
 	router := gin.New()
 
-	//router.Use(gin.Logger())
-	//router.Use(gin.Recovery())
+	router.Use(gin.Logger())
+	router.Use(gin.Recovery())
 
 	router.Use(static.Serve("/", static.LocalFile("./src", true)))
 
-	router.GET("/", func(c *gin.Context) {
-		//c.Header("Content-Type", "application/json")
-		//c.JSON(200, hist)
-	})
-	router.POST("/calculator-post", func(c *gin.Context) {
-		mathToClients := c.PostForm("mathToServer")
-		c.JSON(
-			200,
-			gin.H{"mathToClients": html.EscapeString(mathToClients)},
-		)
-	})
+	router.GET("/", handleMainGet)
+	router.POST("/calculator-post", handlePost)
+	router.GET("/stream", handleStream)
 
 /*
 	// Apply the middleware to the router (works with groups too)
@@ -61,20 +60,60 @@ func InitRoutes() *gin.Engine {
 	return router
 }
 
+func handleMainGet(c *gin.Context) {
+
+}
+
+func handlePost(c *gin.Context) {
+	mathToClients := c.PostForm("mathToServer")
+	if mathLogIdx < MATH_LOG_SIZE {
+		mathLog[mathLogIdx] = mathToClients
+		mathLogIdx++
+	} else {
+		//enqueue at last index, dequeue index 0
+		mathLogAppended := append(mathLog[1:MATH_LOG_SIZE], mathToClients)
+		copy(mathLog[:], mathLogAppended)
+	}
+	mathLogUpdated = true
+	fmt.Println(mathLog)
+	c.JSON(
+		200,
+		gin.H{"mathToClients": mathToClients},
+	)
+}
+
+func handleStream(c *gin.Context) {
+	chanStream := make(chan string)
+	go func() {
+		defer close(chanStream)
+		if mathLogUpdated {
+			mathLogUpdated = false
+			chanStream <- "START\n"
+			for i:= 0; i < MATH_LOG_SIZE; i++ {
+				if len(mathLog[i]) == 0 {
+					break;
+				}
+				appendedMathLog := make([]string, 100)
+				appendedMathLog = append(appendedMathLog, "\n")
+				chanStream <- mathLog[i]
+				time.Sleep(time.Millisecond *1)
+			}
+			chanStream <- "END\n"
+		} else {
+			chanStream <- "IGNORE\n"
+		}
+		time.Sleep(time.Second *1)
+	}()
+	c.Stream(func(w io.Writer) bool {
+		if msg, ok := <-chanStream; ok {
+			c.SSEvent("message", msg)
+			return true
+		}
+		return false
+	})
+}
+
 func setUserRoutes(g *gin.RouterGroup) {
-	g.Use(static.Serve("/", static.LocalFile("./src", true)))
-	g.GET("/", func(c *gin.Context) {
-		//c.Header("Content-Type", "application/json")
-		//c.JSON(200, hist)
-	})
-	g.POST("/calculator-post", func(c *gin.Context) {
-		mathToClients := c.PostForm("mathToServer")
-		fmt.Println(mathToClients)
-		c.JSON(
-			200,
-			gin.H{"mathToClients": html.EscapeString(mathToClients)},
-		)
-	})
 }
 
 func setDebugRoutes(g *gin.RouterGroup) {
--- src/App.js
+++ src/App.js
@@ -26,8 +26,23 @@ class Calculator extends React.Component {
 		super(props);
 		this.state = {
 			mathLog: [],
+			newMathLog: []
+			reading: false,
 		}
 		this.submit = this.submit.bind(this);
+		let source = new EventSource("http://localhost:10000/stream");
+		source.addEventListener('message', function(e) {
+		  console.log("sse: ", e.data);
+			if (e.data === "START") {
+				this.setState({reading: true, newMathLog: []})
+			}
+			else if (e.data === "IGNORE") {
+				this.setState({reading: false})
+			}
+			else if (this.state.reading) {
+				this.state.newMathLog.push(e.data)
+			}
+		}, false);
 	}
 
 	submit(e) {
@@ -36,18 +51,18 @@ class Calculator extends React.Component {
 		$.post(
 			"/calculator-post/",
 			{mathToServer: mathMessage},
-			(function(data) {
+			/*(function(data) {
 				this.state.mathLog.push(data.mathToClients);
 				this.setState({mathLog: this.state.mathLog});
 			}).bind(this),
-			"json"
+			"json"*/
 		);
 	}
 	
   render() {
     return (
 			<div>
-				<h1>Calculater</h1>
+				<h1>Calculator</h1>
 				<form autocomplete="off" onSubmit={this.submit}>
 					<input type="text" name="mathInput" placeholder="feed me math"/>
 					<input type="submit" value="="/>
